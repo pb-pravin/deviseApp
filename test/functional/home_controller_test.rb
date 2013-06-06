342af0139f563d165b64e16263b6056918e31219
Peter Spierenburg <peter.spierenburg@mmspos.com>
2013-06-06 08:08:26 -0300

    Added functional test demonstrating problem.

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::TestHelpers
+
+  test "devise shouldnt call to_key on model class" do
+
+    user = User.new
+    user.org_id = 'org_id'
+    user.term_id = 'org_id'
+    user.id = 'username'
+    user.password = 'password'
+
+    sign_in user
+  end
 end
